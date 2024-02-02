2e27cc72c1ef00748adf4861a24ddf266ed9bf64
ummetarin <ummehomairatarin@gmail.com>
2024-02-02 15:41:13 +0600

    just initialize all needed variables

--- LCS.cpp
+++ LCS.cpp
@@ -3,6 +3,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+   
+   int i,j,m,n,to_cost=0;
+   string x,y;
+   m=x.size();
+   n=y.size();
+   int c[m+1][n+1];
+   char b[m+1][n+1];
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
